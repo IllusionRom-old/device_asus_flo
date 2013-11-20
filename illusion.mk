0a34d8174967ef787a9eda2cf364294852921bea
aznrice <aznrice2k4@gmail.com>
2013-11-20 11:11:12 +1100

    Illusion

---
+++ illusion.mk
@@ -0,0 +1,18 @@
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1920
+TARGET_SCREEN_WIDTH := 1200
+
+$(call inherit-product, device/asus/flo/full_flo.mk)
+
+$(call inherit-product, vendor/illusion/config/common.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := flo
+PRODUCT_NAME := illusion_flo
+PRODUCT_BRAND := google
+PRODUCT_MODEL := Nexus 7
+PRODUCT_MANUFACTURER := asus
+
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=razor BUILD_FINGERPRINT=google/razor/flo:4.4/KRT16O/907817:user/release-keys PRIVATE_BUILD_DESC="razor-user 4.4 KRT16O 907817 release-keys"
+
