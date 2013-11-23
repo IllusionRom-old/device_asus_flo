c316cc98656abe970abe79f9d9fdef73008ef349
aznrice <aznrice2k4@gmail.com>
2013-11-22 22:39:33 -0600

    update illusion.mk
    Change-Id: If8cd140d3503e22a6005c9626aaa2c62bded7a6d

--- illusion.mk
+++ illusion.mk
@@ -3,6 +3,8 @@
 TARGET_SCREEN_HEIGHT := 1920
 TARGET_SCREEN_WIDTH := 1200
 
+PRODUCT_RELEASE_NAME := flo
+
 $(call inherit-product, device/asus/flo/full_flo.mk)
 
 $(call inherit-product, vendor/illusion/config/common.mk)
@@ -14,5 +16,8 @@ PRODUCT_BRAND := google
 PRODUCT_MODEL := Nexus 7
 PRODUCT_MANUFACTURER := asus
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=razor BUILD_FINGERPRINT=google/razor/flo:4.4/KRT16O/907817:user/release-keys PRIVATE_BUILD_DESC="razor-user 4.4 KRT16O 907817 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=razor BUILD_FINGERPRINT="google/razor/flo:4.4/KRT16O/907817:user/release-keys" PRIVATE_BUILD_DESC="razor-user 4.4 KRT16O 907817 release-keys"
 
+# bootanimation
+PRODUCT_COPY_FILES += \
+	vendor/illusion/bootanimation/Illusion1080x1080.zip:system/media/bootanimation.zip
